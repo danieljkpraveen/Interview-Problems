7e7a98ba858d31d777953cacefd1160ce8fc5718
danieljkpraveen <danieljkpraveen@gmail.com>
2024-06-20 11:54:35 +0530

    small notes

--- 24_06_18/bookstore/bookstore/serializers.py
+++ 24_06_18/bookstore/bookstore/serializers.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
+        """
+        defines how the class behaves
+        we define the configs for the serializer class
+        - what model is the data source
+        - what data to get from the source
+        """
         model = User
         fields = [
             "id",
